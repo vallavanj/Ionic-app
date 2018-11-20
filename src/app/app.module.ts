d97542d8abd2067087d5a407c0c16572111be7f7
Vallavan j <vallavan.j@ocsmain.com>
2018-11-20 11:02:51 +0530

    updated the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import {HttpClientModule} from '@angular/common/http';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -25,6 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/itinerary-detail/itinerary-detail.ts
+++ src/pages/itinerary-detail/itinerary-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 /**
  * Generated class for the ItineraryDetailPage page.
  *
@@ -15,12 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ItineraryDetailPage {
 	id:number;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient) {
   	   this.id = navParams.get('id');
   }
-
+	public getJSON(): Observable<any> {
+        return this.http.get("assets/docs/itenarydetail.json",{responseType :"text"});
+    }
+      get_data:any[];
   ionViewDidLoad() {
    // console.log(this.id);
+
+		this.getJSON().subscribe(data => {
+            let id:number =this.id;
+            let Json_data = JSON.parse(data);
+            let itneray_detail:any[]=[];
+            itneray_detail.push(Json_data[id]);
+             this.get_data=itneray_detail;
+             console.log(itneray_detail);
+           //console.log(this.get_data['0']['sub_content'] );
+        });
+
   }
 
 }
